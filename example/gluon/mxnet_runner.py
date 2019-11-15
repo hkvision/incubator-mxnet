dc1d7865e2ecd8dab50d5de1e3d909ad7775c80d
Kai Huang <huangkaivision@gmail.com>
2019-11-15 13:47:53 +0800

    first version of ray example

--- example/gluon/mxnet_runner.py
+++ example/gluon/mxnet_runner.py
@@ -9,9 +9,10 @@ import os
 class MXNetRunner(object):
     """Manages a MXNet model for training."""
 
-    def __init__(self, model_creator, data_creator, args):
-        self.model_creator = model_creator
+    def __init__(self, data_creator, model_creator, loss_creator, args):
         self.data_creator = data_creator
+        self.model_creator = model_creator
+        self.loss_creator = loss_creator
         self.args = args
         self.is_worker = False
         self.epoch = 0
@@ -26,6 +27,16 @@ class MXNetRunner(object):
             os.environ.update(env)
             import mxnet as mx
             self.kv = mx.kv.create(self.args.kvstore)
+            self.train_dataset, self.test_dataset = self.data_creator(self.args, self.kv)
+            self.model = self.model_creator(self.args)
+            self.loss = self.loss_creator(self.args)
+            from mxnet import gluon
+            self.trainer = gluon.Trainer(self.model.collect_params(), 'sgd',
+                                         optimizer_params={'learning_rate': self.args.lr,
+                                                           'wd': self.args.wd,
+                                                           'momentum': self.args.momentum,
+                                                           'multi_precision': True},
+                                         kvstore=self.kv)
             print(self.kv.num_workers)
             print(self.kv.rank)
         else:  # server or scheduler
@@ -33,37 +44,38 @@ class MXNetRunner(object):
 
     def step(self):
         """Runs a training epoch and updates the model parameters."""
-        import time
-        tic = time.time()
-        self.train_data.reset()
-        from mxnet.metric import Accuracy, TopKAccuracy, CompositeEvalMetric
-        metric = CompositeEvalMetric([Accuracy(), TopKAccuracy(5)])
-        metric.reset()
-        btic = time.time()
-        for i, batch in enumerate(self.train_data):
-            from mxnet import gluon
-            data = gluon.utils.split_and_load(batch.data[0].astype("float32"), ctx_list=[mx.cpu()], batch_axis=0)
-            label = gluon.utils.split_and_load(batch.label[0].astype("float32"), ctx_list=[mx.cpu()], batch_axis=0)
-            outputs = []
-            Ls = []
-            from mxnet import autograd as ag
-            with ag.record():
-                for x, y in zip(data, label):
-                    z = self.net(x)
-                    L = self.loss(z, y)
-                    # store the loss and do backward after we have done forward
-                    # on all GPUs for better speed on multiple GPUs.
-                    Ls.append(L)
-                    outputs.append(z)
-                ag.backward(Ls)
-            self.trainer.step(batch.data[0].shape[0])
-            metric.update(label, outputs)
-            if self.args.log_interval and not (i + 1) % self.opt.log_interval:
-                name, acc = metric.get()
-                import logging
-                logger.info('Epoch[%d] Batch [%d]\tSpeed: %f samples/sec\t%s=%f, %s=%f' % (
-                    self.epoch, i, self.args.batch_size / (time.time() - btic), name[0], acc[0], name[1], acc[1]))
+        if self.is_worker:
+            import time
+            tic = time.time()
+            self.train_dataset.reset()
+            from mxnet.metric import Accuracy, TopKAccuracy, CompositeEvalMetric
+            metric = CompositeEvalMetric([Accuracy(), TopKAccuracy(5)])
+            metric.reset()
             btic = time.time()
+            for i, batch in enumerate(self.train_dataset):
+                import mxnet as mx
+                from mxnet import gluon
+                data = gluon.utils.split_and_load(batch.data[0].astype("float32"), ctx_list=[mx.cpu()], batch_axis=0)
+                label = gluon.utils.split_and_load(batch.label[0].astype("float32"), ctx_list=[mx.cpu()], batch_axis=0)
+                outputs = []
+                Ls = []
+                from mxnet import autograd as ag
+                with ag.record():
+                    for x, y in zip(data, label):
+                        z = self.model(x)
+                        L = self.loss(z, y)
+                        # store the loss and do backward after we have done forward
+                        # on all GPUs for better speed on multiple GPUs.
+                        Ls.append(L)
+                        outputs.append(z)
+                    ag.backward(Ls)
+                self.trainer.step(batch.data[0].shape[0])
+                metric.update(label, outputs)
+                if self.args.log_interval and not (i + 1) % self.args.log_interval:
+                    name, acc = metric.get()
+                    print('Epoch[%d] Batch [%d]\tSpeed: %f samples/sec\t%s=%f, %s=%f' % (
+                        self.epoch, i, self.args.batch_size / (time.time() - btic), name[0], acc[0], name[1], acc[1]))
+                btic = time.time()
 
     def shutdown(self):
         """Attempts to shut down the worker."""
@@ -90,11 +102,13 @@ def find_free_port():
 
 class MXNetTrainer(object):
     def __init__(self,
-                 model_creator,
                  data_creator,
+                 model_creator,
+                 loss_creator,
                  args):
-        self.model_creator = model_creator
         self.data_creator = data_creator
+        self.model_creator = model_creator
+        self.loss_creator = loss_creator
         self.args = args
         self.num_workers = args.num_workers
         self.num_servers = args.num_servers if args.num_servers else self.num_workers
@@ -106,8 +120,9 @@ class MXNetTrainer(object):
         # Start workers
         self.runners = [
             Runner.remote(
-                model_creator,
                 data_creator,
+                model_creator,
+                loss_creator,
                 self.args)
             for i in range(self.num_runners)
         ]
@@ -138,3 +153,7 @@ class MXNetTrainer(object):
             worker.setup_distributed.remote(envs[i])
             for i, worker in enumerate(self.runners)
         ])
+
+    def train(self):
+        """Runs a training epoch."""
+        ray.get([w.step.remote() for w in self.runners])
