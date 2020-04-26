aa503b7d267c6d1cfc22fc4dee2babc8ca131a2e
Kai Huang <huangkaivision@gmail.com>
2020-04-26 15:31:32 +0800

    update

--- performance/run.sh
+++ performance/run.sh
@@ -1,1 +1,1 @@
-KMP_AFFINITY=granularity=fine,noduplicates,compact,1,0 OMP_NUM_THREADS=28 numactl --physcpubind=0-27 --membind=0 python dummy_transformer.py
+KMP_AFFINITY=granularity=fine,noduplicates,compact,1,0 OMP_NUM_THREADS=28 numactl --physcpubind=0-27 --membind=0 python transformer.py
